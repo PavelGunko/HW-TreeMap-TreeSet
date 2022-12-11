4cfedfb81ec81fdc7b6199e1fc1280ae73903429
PavelGunko <g9636406069@yandex.ru>
2022-12-11 14:42:55 +0300

    HW-TreeMap-TreeSet

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,24 @@
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) {
         List<Person> Persons = new ArrayList<>();
-        Persons.add(new Person("Gaius Julius ", "Caligula", 45));
+        Persons.add(new Person("Gaius", "Iulius Caesar Augustus Germanicus Caligula ", 45));
         Persons.add(new Person("Claudius", "Ptolemy", 30));
-        Persons.add(new Person("Julius", "Gaius", 50));
-        Persons.add(new Person("Lucius", "Nero", 20));
-        Persons.add(new Person("Marcus", "Aurelius", 30));
+        Persons.add(new Person("Julius", "Gaius Caesar ", 50));
+        Persons.add(new Person("Lucius", "Lucius Domitius Ahenobarbus Nero", 16));
+        Persons.add(new Person("Marcus", "Aurelius Antoninus", 17));
 
 
-        //Comparator Person = new PersonComparator();
         Collections.sort(Persons, new PersonComparator());
         for (Person Person : Persons) {
 
-
             System.out.println(Person);
+
+
         }
+
+
     }
 }
-
-
-
